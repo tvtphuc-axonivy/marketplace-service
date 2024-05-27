66eb58ef7d4b4732281a664e279b115429f79cf9
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2024-05-27 14:40:23 +0700

    MARP-275 Handle Feedback

--- src/main/java/com/axonivy/market/config/WebConfig.java
+++ src/main/java/com/axonivy/market/config/WebConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static com.axonivy.market.Constants.USER_MAPPING;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new HeaderInterceptor()).addPathPatterns("/controller");
+        registry.addInterceptor(new HeaderInterceptor()).addPathPatterns(USER_MAPPING);
     }
 }
